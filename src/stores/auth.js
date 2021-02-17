005611cfcab8bb0004885f3f7bcd3cb58954113f
zhazhahui <1376393028@qq.com>
2021-02-17 17:40:49 +0800

    epic

---
+++ src/stores/auth.js
@@ -0,0 +1,56 @@
+import { observable, action } from 'mobx';
+import { Auth } from '../models';
+import UserStore from './user';
+import { message } from 'antd';
+
+class AuthStore {
+  @observable values = {
+    username: '',
+    password: ''
+  };
+
+  @action setUsername(username) {
+    this.values.username = username;
+  }
+
+  @action setPassword(password) {
+    this.values.password = password;
+  }
+
+  @action login() {
+    return new Promise((resolve, reject) => {
+      Auth.login(this.values.username, this.values.password)
+      .then(user => {
+        UserStore.pullUser();
+        resolve(user);
+      }).catch(err => {
+        UserStore.resetUser();
+        message.error('登录失败');
+        reject(err);
+      })
+    });
+  }
+
+  @action register() {
+    return new Promise((resolve, reject) => {
+      Auth.register(this.values.username, this.values.password)
+      .then(user => {
+        UserStore.pullUser();
+        resolve(user);
+      }).catch(err => {
+        UserStore.resetUser();
+        message.error('注册失败');
+        reject(err);
+      })
+    });  
+  }
+
+  @action logout() {
+    Auth.logout();
+    UserStore.resetUser();
+  }
+
+}
+
+
+export default new AuthStore();
---
+++ src/stores/index.js
@@ -0,0 +1,18 @@
+import React, { createContext, useContext } from 'react';
+import AuthStore from './auth';
+import UserStore from './user';
+import ImageStore from './image';
+
+const context = createContext({
+  AuthStore,
+  UserStore,
+  ImageStore
+});
+
+window.stores = {
+  AuthStore,
+  UserStore,
+  ImageStore
+};
+
+export const useStores = () => useContext(context);
